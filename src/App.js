2cd670c41341aa0a20aada4bc8fc84c7201d0b50
Kushagr Narayan <nkushagr@gmail.com>
2023-08-21 00:15:13 +0530

    fr_1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import io from 'socket.io-client'
 import React from "react"
+import Chats from './components/Chats';
 
 const socket=io.connect("http://localhost:3001")
 
 function App() {
 
-  const [message, setMessage] = React.useState("")
-  const [receivedM, setReceivedM] = React.useState("")
   const [room, setRoom] = React.useState("")
+
 
   const joinRoom=()=>{
     if(room!==""){
@@ -16,36 +16,20 @@ function App() {
     }
   };
 
-  const sendMessage=()=>{
-    if(message!=="")
-    socket.emit("send_m",{message,room})
-  };
-
-  // Listen for upcoming data
-  React.useEffect(() => {
-  socket.on("receive_m",(data)=>{
-    setReceivedM(data.message)
-  })
-  }, [socket])
-
+  
 
   return (
     <div className="App">
-
+      <h1>
+        {socket.id}
+      </h1>
 <input placeholder='Room number:' 
      onChange={(event)=>{
       setRoom(event.target.value)
      }}/>
      <button onClick={joinRoom}>Join Room</button>
 
-     <input placeholder='Enter Message' 
-     onChange={(event)=>{
-      setMessage(event.target.value)
-     }}/>
-     <button onClick={sendMessage}>Send Message</button>
-
-     <h1>Messages:</h1>
-     {receivedM}
+     <Chats socket={socket} room={room} />
     </div>
   );
 }
---
+++ src/components/Chats.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function Chats(props) {
+  const [receivedM, setReceivedM] = React.useState([])
+  const [message, setMessage] = React.useState("")
+
+  // Listen for upcoming data
+  React.useEffect(() => {
+    props.socket.on("receive_m",(data)=>{
+      setReceivedM((list)=>[...list, data]);
+    });
+    return()=>{
+      props.socket.off("recieve_m").off();
+    }
+    }, [props.socket])
+
+  
+  const sendMessage=async()=>{
+    if(message!==""){
+      const MessageData={
+        room: props.room,
+        author:props.socket.id,
+        message:message
+      };
+    await 
+    props.socket.emit("send_m",MessageData);
+    setReceivedM((list)=>[...list, MessageData]);
+    setMessage("");
+    }
+  };
+
+  
+  
+
+
+  return (
+    <div>
+        <input placeholder='Enter Message' 
+     onChange={(event)=>{
+      setMessage(event.target.value)
+     }}/>
+     <button onClick={sendMessage}>Send Message</button>
+       <h1>Messages:</h1>
+     {receivedM.map((message)=>{
+      return <h1>{message.author}$_{message.message}</h1>;
+     })}
+    </div>
+  )
+}
+
+export default Chats
