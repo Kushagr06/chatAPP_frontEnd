c10c600c093e481d2f37313fe13a59893b99664e
Kushagr Narayan <nkushagr@gmail.com>
2023-09-11 02:41:41 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -61,15 +61,16 @@ function App() {
   
 
   return (
-    <div className="App text-2xl bg-gray-900 -mt-2">
-      <div className=' flex justify-around m-2 mt-1 mx-10'>
+    <div className="App text-2xl bg-gray-900 m-0 flex-col min-h-[100vh] p-0 ">
+      <div className=' flex justify-around mx-10'>
 
       <div className='relative bg-gray-800 h-20 w-20 text-emerald-400 text-center p-5 text-xl font-semibold border-2 border-emerald-400 rounded-xl m-2 duration-300 mt-10'>
-      <svg className='fixed ml-2' height="45px" width="45px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve" fill="#34D399"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">  <g> <path class="st0" d="M341.942,356.432c-20.705-12.637-28.134-11.364-28.134-36.612c0-8.837,0-25.256,0-40.403 c11.364-12.62,15.497-11.049,25.107-60.597c19.433,0,18.174-25.248,27.34-47.644c7.471-18.238,1.213-25.632-5.08-28.654 c5.144-66.462,5.144-112.236-70.292-126.436c-27.344-23.437-68.605-15.48-88.158-11.569c-19.536,3.911-37.159,0-37.159,0 l3.356,31.49c-28.608,34.332-14.302,80.106-18.908,106.916c-6.002,3.27-11.416,10.809-4.269,28.253 c9.165,22.396,7.906,47.644,27.34,47.644c9.61,49.548,13.742,47.977,25.107,60.597c0,15.147,0,31.566,0,40.403 c0,25.248-8.581,25.683-28.133,36.612c-47.14,26.349-108.569,41.658-119.575,124.01C48.468,495.504,134.952,511.948,256,512 c121.048-0.052,207.528-16.496,205.517-31.558C450.511,398.09,388.519,384.847,341.942,356.432z"></path> </g> </g></svg>
-      <h1  div className='fixed text-emerald-400 '> {num===""?0:num}</h1>
+
+      <svg className='absolute ml-2' height="45px" width="45px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve" fill="#34D399"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">  <g> <path class="st0" d="M341.942,356.432c-20.705-12.637-28.134-11.364-28.134-36.612c0-8.837,0-25.256,0-40.403 c11.364-12.62,15.497-11.049,25.107-60.597c19.433,0,18.174-25.248,27.34-47.644c7.471-18.238,1.213-25.632-5.08-28.654 c5.144-66.462,5.144-112.236-70.292-126.436c-27.344-23.437-68.605-15.48-88.158-11.569c-19.536,3.911-37.159,0-37.159,0 l3.356,31.49c-28.608,34.332-14.302,80.106-18.908,106.916c-6.002,3.27-11.416,10.809-4.269,28.253 c9.165,22.396,7.906,47.644,27.34,47.644c9.61,49.548,13.742,47.977,25.107,60.597c0,15.147,0,31.566,0,40.403 c0,25.248-8.581,25.683-28.133,36.612c-47.14,26.349-108.569,41.658-119.575,124.01C48.468,495.504,134.952,511.948,256,512 c121.048-0.052,207.528-16.496,205.517-31.558C450.511,398.09,388.519,384.847,341.942,356.432z"></path> </g> </g></svg>
+      <h1  div className='absolute text-emerald-400 '> {num===""?0:num}</h1>
       </div>
 
-      <div className='flex-col justify-center bg-gray-800 h-24 w-auto text-emerald-400 text-center p-5 text-xl font-semibold border-2 border-emerald-400 rounded-xl m-2 duration-300 mt-10'>
+      <div className=' bg-gray-800 h-20 w-auto text-emerald-400 text-center p-5 text-xl font-semibold border-2 border-emerald-400 rounded-xl m-2 duration-300 mt-10'>
       <input className='bg-gray-600' placeholder=' Enter User Name:' 
      onChange={(event)=>{
       setUserName(event.target.value)
@@ -80,18 +81,25 @@ function App() {
      </div>
       
       <h1 className='flex justify-start bg-gray-800 h-20 w-auto text-emerald-400 text-center p-5 text-xl font-semibold border-2 border-emerald-400 rounded-xl m-2 duration-300 mt-10'>Current Room:&nbsp; 
-      {roomno===""?"null":  roomno}
+      {roomno===""?"Lobby":  roomno}
       </h1>
           
      </div>
-     
-     
+       
+      <div className='flex justify-center m-5 mt-10' >
 
-      {/* chat logs */}
-      
-      <div className='flex justify-center m-4' id="ChatLog">
+         {/* Join Room */}
+     <div className='flex justify-center bg-gray-800 h-24 w-1/2 text-emerald-400 text-center p-5 text-xl font-semibold border-2 border-emerald-400 rounded-xl m-2 duration-300 mt-10 align-middle items-center'>
+     <input className='bg-gray-600 mx-2' placeholder='Room number:' 
+     onChange={(event)=>{
+      setRoom(event.target.value)
+     }}/>
+     <button className=' bg-emerald-900 hover:bg-emerald-500 duration-300 text-gray-300 hover:text-gray-200 px-5 py-1  rounded-xl' onClick={joinRoom}>Join Room</button>
+     </div>
+
+        {/* chat logs */}
       <div className=' h-36 w-3/4 bg-gray-800 p-2 rounded-xl text-emerald-400 overflow-auto border-2 border-emerald-400 text-left'>
-      <h2>Chat Logs:</h2>
+      <h2 className='border-2 p-2 border-emerald-400 rounded-xl'>Chat Logs:</h2>
       <div className='mt-2 text-center'>
       {id.map((message)=>{
       return <h4>{'=> : '+ message}</h4>;
@@ -101,11 +109,7 @@ function App() {
      </div>
 
   
-<input placeholder='Room number:' 
-     onChange={(event)=>{
-      setRoom(event.target.value)
-     }}/>
-     <button className='bg-blue-400 text-gray-300 px-5' onClick={joinRoom}>Join Room</button>
+
      
 
      <Chats socket={socket} room={room} user={userName} />
--- src/components/Chats.js
+++ src/components/Chats.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Chats(props) {
   const [receivedM, setReceivedM] = React.useState([])
   const [message, setMessage] = React.useState("")
+  const [style, setStyle]=React.useState({})
 
   // Listen for upcoming data
   React.useMemo(() => {
@@ -33,16 +34,33 @@ function Chats(props) {
 
 
   return (
-    <div className='mt-10'>
-        <input placeholder='Enter Message' 
+    <div className='mt-10 bg-gray-900 flex-col flex-grow max-h-screen'>
+        <input
+        style={style} 
+        placeholder='Enter Message'
+        className='bg-gray-600 mx-2 w-1/2 h-16 border-2 text-gray-100' 
      onChange={(event)=>{
+      if(event.target.value==='')
+        setStyle({border:'1px solid gray'});
+      else
+        setStyle({border:'2px solid #34d399'});
+
       setMessage(event.target.value)
      }}/>
-     <button onClick={sendMessage} className='bg-blue-400 text-gray-300 px-5'>Send Message</button>
-       <h1>Messages:</h1>
+     <button onClick={sendMessage} className=' bg-emerald-900 hover:bg-emerald-500 duration-300 text-gray-300 hover:text-gray-200 px-5 py-1  rounded-xl' >
+
+     <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.7639 12H10.0556M3 8.00003H5.5M4 12H5.5M4.5 16H5.5M9.96153 12.4896L9.07002 15.4486C8.73252 16.5688 8.56376 17.1289 8.70734 17.4633C8.83199 17.7537 9.08656 17.9681 9.39391 18.0415C9.74792 18.1261 10.2711 17.8645 11.3175 17.3413L19.1378 13.4311C20.059 12.9705 20.5197 12.7402 20.6675 12.4285C20.7961 12.1573 20.7961 11.8427 20.6675 11.5715C20.5197 11.2598 20.059 11.0295 19.1378 10.5689L11.3068 6.65342C10.2633 6.13168 9.74156 5.87081 9.38789 5.95502C9.0808 6.02815 8.82627 6.24198 8.70128 6.53184C8.55731 6.86569 8.72427 7.42461 9.05819 8.54246L9.96261 11.5701C10.0137 11.7411 10.0392 11.8266 10.0493 11.9137C10.0583 11.991 10.0582 12.069 10.049 12.1463C10.0387 12.2334 10.013 12.3188 9.96153 12.4896Z" stroke="#e5e7eb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+     </button>
+
+     {/* Messages */}
+     <div>
+     <div className=' flex-col flex-grow m-10 border-4 min-h-[20vh] h-auto border-emerald-400 rounded-xl  p-2 bg-gray-800 overflow-y-auto max-h-[35vh]'>
+       <h1 className='text-emerald-400 text-left border-2 rounded-xl p-2 border-emerald-400'>Messages:</h1>
      {receivedM.map((message)=>{
       return <h2> <span id={props.user===message.author?'host':'receiver'}>{message.author}{':>: '}{message.message}</span></h2>;
      })}
+    </div>
+    </div>
     </div>
   )
 }
